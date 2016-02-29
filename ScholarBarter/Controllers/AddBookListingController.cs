9d47d69dd04b00af7e1f8a4e24eaf43910ebac89
Dominick Atanasio <dom@domsnet.net>
2016-02-28 22:19:34 -0800

    added a AddBookListingController, Need to define the class that actually does the work. That's for tomorrow

---
+++ ScholarBarter/Controllers/AddBookListingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Helpers;
+using System.Web.Http;
+using ScholarBarter.Models;
+using ScholarBarter.Models.DataContexts;
+using ScholarBarter.Models.Exceptions;
+
+namespace ScholarBarter.Controllers
+{
+    public class AddBookListingController : ApiController
+    {
+        [HttpPost]
+        public string post(FormDataCollection formData)
+        {
+            ListingsManager.insertBookListing(formData);
+
+            return "cool";
+        }
+
+    }
+}
---
+++ ScholarBarter/Models/ListingsManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using ScholarBarter.Models.DataContexts;
+
+namespace ScholarBarter.Models
+{
+    public class ListingsManager
+    {
+        internal static void insertBookListing(FormDataCollection formData){
+            ListingsDataContext dc = new ListingsDataContext();
+
+            Listing list = new Listing();
+
+            list.CreationTime = DateTime.Now;
+            list.Active = false;
+            list.Description = "temp";
+            list.ListingType = "book";
+            list.Price = 45.4m;
+            list.Title = "JABFS - Just Another Book For Sale";
+            list.UserId = 1;
+
+            dc.Listings.InsertOnSubmit(list);
+
+            dc.SubmitChanges();
+        }
+    }
+}
