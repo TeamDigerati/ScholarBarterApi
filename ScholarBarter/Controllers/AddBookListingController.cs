081693921f8774a6ab52e7ce4308e97df83b05a4
Dominick Atanasio <dom@domsnet.net>
2016-02-29 10:13:23 -0800

    Created a lisings manager to handle some of the crud operations Added an insert book listing method and tested
    In typical Microsoft fasion, using a transaction did not work
    received timeout messages
    nor does the default value for the bit field that they using for boolean
    
    instead of using the inbuilt transaction mechanism I delete an already
    created record operation from within the catch statement of a
    record insert that fails

--- ScholarBarter/Controllers/AddBookListingController.cs
+++ ScholarBarter/Controllers/AddBookListingController.cs
@@ -15,9 +15,18 @@ namespace ScholarBarter.Controllers
         [HttpPost]
         public string post(FormDataCollection formData)
         {
-            ListingsManager.insertBookListing(formData);
+            try
+            {
+                ListingsManager.insertBookListing(formData);
+            }
+            catch (Exception e)
+            {
+                return string.Format(
+                    "Could not add the book. The following error was given: {0}",
+                    e.Message);
+            }
 
-            return "cool";
+            return "Success";
         }
 
     }
--- ScholarBarter/Models/ListingsManager.cs
+++ ScholarBarter/Models/ListingsManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
+using System.Reflection;
 using System.Web;
 using ScholarBarter.Models.DataContexts;
 
@@ -9,22 +10,42 @@ namespace ScholarBarter.Models
 {
     public class ListingsManager
     {
-        internal static void insertBookListing(FormDataCollection formData){
-            ListingsDataContext dc = new ListingsDataContext();
+        internal static void insertBookListing(FormDataCollection fd)
+        {
+            {
+                ListingsDataContext lc = new ListingsDataContext();
+                               
+                Listing lstng = new Listing();
+                lstng.CreationTime = DateTime.Now;
+                lstng.Active = true;
+                lstng.Description = fd.Get("Description");
+                lstng.ListingType = "book";
+                lstng.Price = Convert.ToDecimal(fd.Get("Price"));
+                lstng.Title = fd.Get("Title");
+                lstng.UserId = Convert.ToInt32(fd.Get("UserId"));
 
-            Listing list = new Listing();
+                lc.Listings.InsertOnSubmit(lstng);
+                lc.SubmitChanges();
 
-            list.CreationTime = DateTime.Now;
-            list.Active = false;
-            list.Description = "temp";
-            list.ListingType = "book";
-            list.Price = 45.4m;
-            list.Title = "JABFS - Just Another Book For Sale";
-            list.UserId = 1;
+              try
+                {   
+                    BooksDataContext bc = new BooksDataContext();
 
-            dc.Listings.InsertOnSubmit(list);
-
-            dc.SubmitChanges();
+                    Book bk = new Book();
+                    bk.ListingId = lstng.ListingId;
+                    bk.Condition = fd.Get("Condition");
+                    bk.Isbn10 = fd.Get("Isbn10");
+                    
+                    bc.Books.InsertOnSubmit(bk);
+                    bc.SubmitChanges();
+                }
+                catch (Exception)
+                {
+                    lc.Listings.DeleteOnSubmit(lstng);
+                    lc.SubmitChanges();
+                    throw;
+                }
+            }
         }
     }
 }
